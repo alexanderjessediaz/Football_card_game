2c203920e406e49f678c1a8922933b7e559c743d
AJ <alexanderjdiaz89@gmail.com>
2020-04-22 14:00:00 -0600

    rework auth controller, made jackie, jackie123 user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,18 +3,10 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by(username: params[:username])
 
-        if !@user
+        if !@user or !@user.authenticate params[:password]
             render status: :unauthorized
         else 
-            if !user.authenticate(params[:password])
-                render status: :unauthorized
-            else
-                secret = Rails.application.secret_key_base
-                payload = {user_id: @user.id}
-                token = JWT.encode(payload, secret)
-                
-                render json: (token: token), status: :ok
-            end
+            render status: :ok
         end
-    end
+    end 
 end
